f9b72c85c81ec4266fc43da71608e9a6cc7aa92f
Chris Porter <chris.porter@um.edu.mt>
2024-03-18 15:08:16 +0100

    First iteration

--- main.js
+++ main.js
@@ -1,32 +1,98 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
+const { app, BrowserWindow, BrowserView, ipcMain } = require('electron')
 const path = require('path')
+const { log } = require('electron-log');
 
 let mainWindow
+let browserView;
 // const iconPath = path.join(__dirname, 'logo.png')
 
 function createWindow () {
-  mainWindow = new BrowserWindow({ 
-    //https://www.electronjs.org/docs/latest/tutorial/security
-    webPreferences: {
-      nodeIntegration: true, 
-      nodeIntegrationInWorker: true,
-      contextIsolation: false,
-      webviewTag: true,
-      preload: path.join(__dirname, 'preload.js'),
+  try {
+    mainWindow = new BrowserWindow({ 
+      //https://www.electronjs.org/docs/latest/tutorial/security
+      webPreferences: {
+        nodeIntegrationInWorker: true,
+        contextIsolation: false,
+        // preload: path.join(__dirname, 'preload.js'),
+        preload: path.join(__dirname, '/render-mainwindow.js'), // Set the preload script
+        icon: __dirname + '/AppIcon.icns'
+      },
       icon: __dirname + '/AppIcon.icns'
-    },
-    icon: __dirname + '/AppIcon.icns'
+    })
     
-  })
+    //Create browser view
+    browserView = new BrowserView({ 
+      //https://www.electronjs.org/docs/latest/tutorial/security
+      webPreferences: {
+        nodeIntegrationInWorker: true,
+        contextIsolation: false,
+        preload: path.join(__dirname, '/render-browserview.js')
+      },
+      icon: __dirname + '/AppIcon.icns'
+    });
+    
+    mainWindow.maximize();
+    mainWindow.loadURL(path.join(__dirname, '/index.html')).then(() => {
+      mainWindow.webContents.send('mainWindowLoaded');
+      mainWindow.webContents.openDevTools();
 
-  mainWindow.maximize();
-  mainWindow.loadURL('file://' + __dirname + '/index.html')
+      //Once the main page is loaded, create inner browserview and place it in the right position by getting the x,y,width,height of a positioned element in index.html
+      mainWindow.webContents.executeJavaScript(`
+        (() => {
+          const element = document.querySelector('#browserViewContainer');
+          if (element) {
+            const rect = element.getBoundingClientRect();
+            return {
+              x: rect.left,
+              y: rect.top,
+              width: rect.width,
+              height: rect.height
+            };
+          } else {
+            return null;
+          }
+        })()
+      `)
+      .then(properties => {
+        //Attach the browser view to the parent window
+        mainWindow.setBrowserView(browserView);
+        
+        //Set its location/dimensions as per the returned properties
+        browserView.setBounds({ x: Math.floor(properties.x), y: Math.floor(properties.y), width: Math.floor(properties.width), height: Math.floor(properties.height) });
+        //Set auto resize
+        browserView.setAutoResize({
+          width: true,
+          height: true, 
+          horizontal: true,
+          vertical: false
+        });
+        //Load the default home page
+        browserView.webContents.loadURL('https://www.accessibility.com.mt');
 
-  mainWindow.webContents.openDevTools()
-  
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
+        //Once the DOM is ready, send a message to initiate some further logic
+        browserView.webContents.on('dom-ready', () => {
+          // This event fires when the BrowserView is attached
+          browserView.webContents.send('browserViewLoaded');
+          browserView.webContents.insertCSS('html, body, a, input, button, div { cursor: none; }')
+          browserView.webContents.openDevTools();
+        });
+        
+      })
+      .catch(err =>
+      {
+        log.error(err);
+      }); 
+    })
+
+    //mainWindow.webContents.openDevTools()
+    
+    mainWindow.on('closed', () => {
+      mainWindow = null
+    });
+  }
+  catch(err) {
+    log.error(err);
+  }
 }
 
 app.on('ready', createWindow)
@@ -37,11 +103,11 @@ app.on('window-all-closed', () => {
   }
 })
 
-app.on('web-contents-created', (event, contents) => {
-  contents.on('will-attach-webview', (event, webPreferences, params) => {
-    webPreferences.nodeIntegration = false
-  })
-})
+// app.on('web-contents-created', (event, contents) => {
+//   contents.on('will-attach-webview', (event, webPreferences, params) => {
+//     webPreferences.nodeIntegration = false
+//   })
+// })
 
 app.on('activate', () => {
   if (mainWindow === null) {
@@ -69,8 +135,9 @@ ipcMain.on('hideScrollUp', () => {
   mainWindow.webContents.send('hideScrollUp')
 })
 
-ipcMain.on('log', (event,log) => {
-  console.log(log)
+ipcMain.on('log', (event,loggedItem) => {
+  log.info(event);
+  log.info(loggedItem);
 });
 
 ipcMain.on('showScrollUp', () => {
---
+++ render-browserview.js
@@ -0,0 +1,22 @@
+const { ipcRenderer }           = require('electron')
+const { createCursor, followCursor } = require('./js/cursor')
+const { byId, readFile, dwell } = require('./js/utils')
+
+let cursor; 
+let browserView;
+
+ipcRenderer.on('browserViewLoaded', () => {
+    createCursor('cursor');
+    cursor = document.getElementById('cursor');
+    followCursor('cursor');
+
+    browserView = document.getRootNode();
+  
+    browserView.addEventListener('mouseout', () => {
+        cursor.style.visibility = 'hidden'
+    })
+
+    browserView.addEventListener('mouseover', () => {
+        cursor.style.visibility = 'visible'
+    })
+  });
