4f6f9fc4b357d6d7c69fdb7b67a9baceb2341dc9
mariebuhagiar <2003buhagiarmarie@gmail.com>
2025-02-26 19:56:42 +0100

    The quadtree and navareas tree are generated when any scroll event is triggered (not merely for the scrolling performed through the scrolling buttons displayed on-screen).

--- app/src/renderer/tabview/render-tabview.js
+++ app/src/renderer/tabview/render-tabview.js
@@ -5,6 +5,7 @@ let cursorInterval;
 let scrollDistance;
 let isScrolling = false;
 let iterator = 0;
+let useNavAreas;
 
 // Create a Trusted Types policy for innerHtml assignments when TrustedHTML policies are set ('This document requires 'TrustedHTML' assignment')
 const policy = window.trustedTypes.createPolicy('default', {
@@ -23,6 +24,7 @@ window.cactusAPI.on('ipc-iframe-loaded', (useNavAreas, scrollDist) => {
 });
 
 window.cactusAPI.on('ipc-main-tabview-loaded', (useNavAreas, scrollDist) => {
+	useNavAreas = useNavAreas;
 	scrollDistance = scrollDist;
 	initScrollableElements(useNavAreas);
 
@@ -247,6 +249,15 @@ window.addEventListener('message', (event) => {
 	}
 });
 
+// This is useful for elements that scroll the page when they are clicked and for any other scrolling 
+// not done through the scrolling buttons displayed on-screen.
+window.addEventListener('scroll', () => {
+    setTimeout(() => {
+		generateQuadTree();
+		if (useNavAreas) generateNavAreasTree();
+	}, 1000);
+});
+
 function createCursor(id) {
 	var cursor = document.createElement('div')
 	var cursorHTML = `<svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="eye" class="fa-eye fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M288 144a110.94 110.94 0 0 0-31.24 5 55.4 55.4 0 0 1 7.24 27 56 56 0 0 1-56 56 55.4 55.4 0 0 1-27-7.24A111.71 111.71 0 1 0 288 144zm284.52 97.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400c-98.65 0-189.09-55-237.93-144C98.91 167 189.34 112 288 112s189.09 55 237.93 144C477.1 345 386.66 400 288 400z"></path></svg>`;
@@ -398,19 +409,7 @@ function initScrollableElements(useNavAreas) {
 
 			function step() {
 				if (!isScrolling) {
-					// clearInterval(quadtreeInterval);
 					return; // Stop if scrolling is interrupted
-				}
-				else {
-					// Start the timer to generate quadtree and navareas
-					if (!quadtreeGeneratorTimer) {
-						quadtreeGeneratorTimer = setTimeout(function () {
-							generateQuadTree();
-							if (useNavAreas) generateNavAreasTree();
-							window.clearTimeout(quadtreeGeneratorTimer);
-							quadtreeGeneratorTimer = null;
-						}, 1000);
-					}
 				}
 
 				checkIfElementIsAtTop(element, scrollUpButton_outerDiv, scrollUpButton);
